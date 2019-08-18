b66503608c465a6ae9662b1eaa0763cd97776dc0
Alexander Lobov <aleksandr.lobov@t-systems.com>
2019-08-18 18:20:19 +0300

    Components. Step 2

--- src/app/parent-child/child/child.component.ts
+++ src/app/parent-child/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./child.component.scss']
 })
 export class ChildComponent {
-  title = 'I am child component';
+  @Input() title = '';
 }
